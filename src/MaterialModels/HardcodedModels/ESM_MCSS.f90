c7d3bec95bca92ff1e471bf607c37bea47b771bf
Jonathan Moore <jonathanm@vt.edu>
2023-12-28 23:24:19 -0500

    Reorganized src folder to match files in Anura3D.sln

--- src/MaterialModels/HardcodedModels/ESM_MCSS.f90
+++ src/MaterialModels/HardcodedModels/ESM_MCSS.f90
@@ -1,3 +1,33 @@
+!*****************************************************************************
+!                                       ____  _____  
+!           /\                         |___ \|  __ \ 
+!          /  \   _ __  _   _ _ __ __ _  __) | |  | |
+!         / /\ \ | '_ \| | | | '__/ _` ||__ <| |  | |
+!        / ____ \| | | | |_| | | | (_| |___) | |__| |
+!       /_/    \_\_| |_|\__,_|_|  \__,_|____/|_____/ 
+!
+!
+!	Anura3D - Numerical modelling and simulation of large deformations 
+!   and soil�water�structure interaction using the material point method (MPM)
+!
+!	Copyright (C) 2023  Members of the Anura3D MPM Research Community 
+!   (See Contributors file "Contributors.txt")
+!
+!	This program is free software: you can redistribute it and/or modify
+!	it under the terms of the GNU Lesser General Public License as published by
+!	the Free Software Foundation, either version 3 of the License, or
+!	(at your option) any later version.
+!
+!	This program is distributed in the hope that it will be useful,
+!	but WITHOUT ANY WARRANTY; without even the implied warranty of
+!	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!	GNU Lesser General Public License for more details.
+!
+!	You should have received a copy of the GNU Lesser General Public License
+!	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!
+!*****************************************************************************
+
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     
  !███╗░░░███╗░█████╗░░██████╗░██████╗
