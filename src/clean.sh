9b56251fd4378d34f74dd31a90fadfd95d6733a8
WaveHello <jonathanm@vt.edu>
2024-10-10 15:57:42 -0400

    Added fpm capability and make file

---
+++ src/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Remove all .mod files in the current directory
+rm -f *.mod
+rm -f *.o
+
+# Optionally, print a message indicating success
+echo "All .mod and .o files have been removed."
